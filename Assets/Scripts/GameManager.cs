7d66eedb765dcf6ed9f1e982550b592f89189447
microtoby2023 <microtoby2023@gmail.com>
2023-06-25 17:31:07 -0400

    Make the Play button clickable.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -17,5 +18,11 @@ public class GameManager : MonoBehaviour
     {
         GameOverCanvas.SetActive(true);
         Time.timeScale = 0f;
+    }
+
+    public void Replay()
+    {
+        Debug.Log("Replay function is called.");
+        SceneManager.LoadScene("scene_fly");
     }
 }
