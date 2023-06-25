fd71b0f72cddce9a1822c34efc599ceb9f9c3cc0
Jerrydw <5678439+jerrydw@users.noreply.github.com>
2023-06-25 15:49:44 -0700

    destroy tube automatically

--- Assets/Scripts/TubeSpawner.cs
+++ Assets/Scripts/TubeSpawner.cs
@@ -1,19 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TubeSpawner : MonoBehaviour
 {
     public GameObject TubePairPrefab;
+    public float OffsetY;
+    public float OffsetX;
+    public float SpawnInterval;
 
     // Start is called before the first frame update
     private void Start()
     {
-        InvokeRepeating("SpawnTube", 1.0f, 1.0f);
+        InvokeRepeating("SpawnTube", SpawnInterval, 1.0f);
     }
 
     private void SpawnTube()
     {
-        Instantiate(this.TubePairPrefab, transform.position, transform.rotation);
+        GameObject newTupe = Instantiate(this.TubePairPrefab, transform.position, transform.rotation);
+        MoveLeft tupeControl = newTupe.GetComponent<MoveLeft>();
+        if (OffsetX > tupeControl.Speed * SpawnInterval / 2) OffsetX = MathF.Max(0, tupeControl.Speed * SpawnInterval / 2 - 0.1f);
+        newTupe.transform.position = transform.position + new Vector3(Random.Range(-OffsetX, OffsetX), Random.Range(-OffsetY, OffsetY), 0);
+        Destroy(newTupe, SpawnInterval * 5);
     }
 }
